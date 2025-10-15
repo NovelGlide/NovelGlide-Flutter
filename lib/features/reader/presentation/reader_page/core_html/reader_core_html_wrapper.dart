5c10e9cc70bc7464fa99d9c2921311ebc2167b2d
Kai <wuvincentck505@gmail.com>
2025-10-16 00:46:31 +0800

    fix: scrolling issue
    Scroll to top after changing page.

--- lib/features/reader/presentation/reader_page/core_html/reader_core_html_wrapper.dart
+++ lib/features/reader/presentation/reader_page/core_html/reader_core_html_wrapper.dart
@@ -4,15 +4,29 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import '../cubit/reader_cubit.dart';
 import 'reader_core_html.dart';
 
-class ReaderCoreHtmlWrapper extends StatelessWidget {
+class ReaderCoreHtmlWrapper extends StatefulWidget {
   const ReaderCoreHtmlWrapper({super.key});
+
+  @override
+  State<ReaderCoreHtmlWrapper> createState() => _ReaderCoreHtmlWrapperState();
+}
+
+class _ReaderCoreHtmlWrapperState extends State<ReaderCoreHtmlWrapper> {
+  final ScrollController _scrollController = ScrollController();
 
   @override
   Widget build(BuildContext context) {
     return SizedBox.expand(
       child: Scrollbar(
         child: SingleChildScrollView(
-          child: BlocBuilder<ReaderCubit, ReaderState>(
+          controller: _scrollController,
+          child: BlocConsumer<ReaderCubit, ReaderState>(
+            listenWhen: (ReaderState previous, ReaderState current) =>
+                previous.htmlContent != current.htmlContent,
+            listener: (BuildContext context, ReaderState state) {
+              // Scroll to top.
+              _scrollController.jumpTo(0.0);
+            },
             buildWhen: (ReaderState previous, ReaderState current) =>
                 previous.htmlContent != current.htmlContent ||
                 previous.readerPreference != current.readerPreference,
@@ -25,5 +39,11 @@ class ReaderCoreHtmlWrapper extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
   }
 }
