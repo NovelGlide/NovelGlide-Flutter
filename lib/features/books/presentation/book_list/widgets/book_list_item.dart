5c295866bd59e4592c7f771a24eba02c8d12edc9
Kai <wuvincentck505@gmail.com>
2025-10-29 23:14:14 +0800

    fix: no size error.

--- lib/features/books/presentation/book_list/widgets/book_list_item.dart
+++ lib/features/books/presentation/book_list/widgets/book_list_item.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:novel_glide/features/books/domain/entities/book_cover.dart';
 
 import '../../../../shared_components/animated_placeholders/ease_flash_placeholder.dart';
+import '../../../../shared_components/shared_list/shared_list.dart';
 import '../../../domain/entities/book.dart';
 import '../../book_cover/book_cover_builder.dart';
+import '../cubit/book_list_cubit.dart';
 import 'book_list_item_book.dart';
 
 class BookListItem extends StatelessWidget {
@@ -13,6 +16,8 @@ class BookListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final BookListCubit cubit = BlocProvider.of<BookListCubit>(context);
+
     return BookCoverBuilder(
       bookData: bookData,
       builder: (_, BookCover coverData) => BookListItemBook(
@@ -20,7 +25,15 @@ class BookListItem extends StatelessWidget {
         coverData: coverData,
       ),
       placeholder: Container(
-        margin: const EdgeInsets.all(16.0),
+        margin: switch (cubit.state.listType) {
+          SharedListType.grid => const EdgeInsets.all(16.0),
+          SharedListType.list => const EdgeInsets.all(4.0),
+        },
+        width: double.infinity,
+        height: switch (cubit.state.listType) {
+          SharedListType.grid => double.infinity,
+          SharedListType.list => 48.0,
+        },
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(24.0),
         ),
